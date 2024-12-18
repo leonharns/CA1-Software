62e458534ce18519c0e8616d17b28b0dce64f8c9
leonharns <50200146+leonharns@users.noreply.github.com>
2024-12-18 12:42:27 +0000

    More sfx added

--- Assets/CarIntroController.cs
+++ Assets/CarIntroController.cs
@@ -9,6 +9,7 @@ public class CarIntroController : MonoBehaviour
     public GameObject RealCar;
     public GameObject Player;
     public AudioClip brokenDown;
+    public AudioSource AS;
     void Start()
     {
         StartCoroutine(SwitchCars());   
@@ -21,7 +22,9 @@ public class CarIntroController : MonoBehaviour
     }
      IEnumerator SwitchCars()
     {
-        yield return new WaitForSeconds(4F);
+        yield return new WaitForSeconds(1.5F);
+        AS.PlayOneShot(brokenDown);
+        yield return new WaitForSeconds(2.5F);
         FakeCar.SetActive(false);
         RealCar.SetActive(true);
         Player.SetActive(true);
